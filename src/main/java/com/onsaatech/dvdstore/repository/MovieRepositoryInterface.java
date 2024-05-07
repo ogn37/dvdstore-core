97670c480cca0883629ccbed6c0218aa85dac636
ogn <ogn@dev.com>
2024-05-07 20:43:08 +0200

    Fetch à priori sur l'acteur principal et les avis niveau repo

--- src/main/java/com/onsaatech/dvdstore/repository/MovieRepositoryInterface.java
+++ src/main/java/com/onsaatech/dvdstore/repository/MovieRepositoryInterface.java
@@ -1,9 +1,15 @@
 package com.onsaatech.dvdstore.repository;
 
 import com.onsaatech.dvdstore.entity.Movie;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface MovieRepositoryInterface extends CrudRepository<Movie,Long> {
+    @Override
+    @EntityGraph(value = "movie.actor-and-reviews", type = EntityGraph.EntityGraphType.FETCH)
+    Optional<Movie> findById(Long aLong);
     /*Movie add(Movie movie);
     Iterable<Movie> geta();
 
